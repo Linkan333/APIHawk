e96dc3591fb7cd09eea69134e98af2af4e1c43ef
Linkan <57046726+Linkan333@users.noreply.github.com>
2025-04-23 08:10:04 +0200

    Update fuzzer.py

--- apihawk/core/fuzzer.py
+++ apihawk/core/fuzzer.py
@@ -5,7 +5,10 @@ import requests
 def fuzz_endpoint(url, wordlist, method, cookies=None, headers=None, timeout=5, proxies=None, verbose=False):
     url = f"{url}/{wordlist}"
     response = requests.get(url, timeout=timeout)
-    if url.endswith("FUZZ"):
-        url.replace("FUZZ", {wordlist})
+    #if url.endswith("FUZZ"):
+        #url.replace("FUZZ", {wordlist})
+    #lay in bed an thought about this problem but a better solution would be following:
+    # this will tho copy the whole wordlist so i am going to make an for line in lines.. functiob
+    url.replace("FUZZ", wordlist)
     if response != 404:
         print(f"Found {url}")
